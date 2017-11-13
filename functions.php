674e350f1d1fa946239315ff8f7f7252889a1033
dexterdr <dexter.gm@gmail.com>
2017-11-14 00:21:23 +0200

    Move scripts to top for plugin minifaction.

--- functions.php
+++ functions.php
@@ -14,9 +14,9 @@ function cosmology_scripts()
     wp_enqueue_style('bootstrap', get_theme_file_uri('/css/bootstrap.min.css'), [], null, 'all');
     wp_enqueue_style('cosmology', get_stylesheet_uri(), [], null, 'all');
 
-    wp_enqueue_script('jquery3', get_theme_file_uri('/js/jquery-3.2.1.min.js'), [], null, true);
-    wp_enqueue_script('popper', get_theme_file_uri('/js/popper.min.js'), [], null, true);
-    wp_enqueue_script('bootstrap', get_theme_file_uri('/js/bootstrap.min.js'), [], null, true);
+    wp_enqueue_script('jquery3', get_theme_file_uri('/js/jquery-3.2.1.min.js'), [], null);
+    wp_enqueue_script('popper', get_theme_file_uri('/js/popper.min.js'), [], null);
+    wp_enqueue_script('bootstrap', get_theme_file_uri('/js/bootstrap.min.js'), [], null);
     // wp_enqueue_script('app', get_theme_file_uri('/js/app.min.js'), [], null, true);
 }
 
