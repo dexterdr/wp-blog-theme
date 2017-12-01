8a29b22725abbc721a9e91f62dd7818e05464f03
dexterdr <dexter.gm@gmail.com>
2017-12-02 00:50:06 +0200

    Better columns for category description.

--- js/_compile.cmd
+++
@@ -1,1 +0,0 @@
-java -jar c:\utils\compiler.jar --compilation_level SIMPLE_OPTIMIZATIONS --js jquery-3.2.1.slim.min.js popper.min.js bootstrap.min.js masonry.min.js main.js --js_output_file ../app.min.js
